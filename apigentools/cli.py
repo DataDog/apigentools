30ffb85c86ca2a38be62154af9779f0729c66f0e
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2019-07-30 17:19:45 -0400

    Remove extra license from file that already had

--- apigentools/cli.py
+++ apigentools/cli.py
@@ -2,10 +2,6 @@
 # under the 3-clause BSD style license (see LICENSE).
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2019 Datadog, Inc.
-# (C) Datadog, Inc. 2019
-# All rights reserved
-# Licensed under a 3-clause BSD style license (see LICENSE)
-
 import argparse
 import logging
 import os
