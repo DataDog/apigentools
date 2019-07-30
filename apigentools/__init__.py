330367dd79598031f032f8cec9086579bf2abfc4
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2019-07-30 17:18:02 -0400

    Add license header to all files

--- apigentools/__init__.py
+++ apigentools/__init__.py
@@ -1,1 +1,5 @@
+# Unless explicitly stated otherwise all files in this repository are licensed
+# under the 3-clause BSD style license (see LICENSE).
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2019 Datadog, Inc.
 __version__ = "0.1.0.dev1"
--- apigentools/commands/__init__.py
+++ apigentools/commands/__init__.py
@@ -1,3 +1,7 @@
+# Unless explicitly stated otherwise all files in this repository are licensed
+# under the 3-clause BSD style license (see LICENSE).
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2019 Datadog, Inc.
 from apigentools.commands.init import InitCommand
 from apigentools.commands.generate import GenerateCommand
 from apigentools.commands.push import PushCommand
