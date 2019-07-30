7abc1e6d794fa6a8b0fde93d9e64df98e8e9a72c
Slavek Kabrda <slavek.kabrda@datadoghq.com>
2019-07-30 11:02:04 +0200

    Add 'init' command that can initialize a basic spec repo scaffold

--- apigentools/commands/__init__.py
+++ apigentools/commands/__init__.py
@@ -1,3 +1,4 @@
+from apigentools.commands.init import InitCommand
 from apigentools.commands.generate import GenerateCommand
 from apigentools.commands.push import PushCommand
 from apigentools.commands.split import SplitCommand
@@ -6,6 +7,7 @@ from apigentools.commands.test import TestCommand
 from apigentools.commands.validate import ValidateCommand
 
 all_commands = {
+    "init": InitCommand,
     "generate": GenerateCommand,
     "split": SplitCommand,
     "templates": TemplatesCommand,
---
+++ apigentools/commands/init.py
@@ -0,0 +1,97 @@
+import json
+import logging
+import os
+import yaml
+
+from apigentools.commands.command import Command
+from apigentools.utils import run_command, change_cwd
+
+from apigentools import constants
+
+log = logging.getLogger(__name__)
+
+
+class InitCommand(Command):
+    def run(self):
+        cmd_result = 0
+        log.info("Initializing a new project directory")
+        is_repo = not self.args.no_git_repo
+
+        os.makedirs(self.args.projectdir, exist_ok=True)
+        with change_cwd(self.args.projectdir):
+            dirs = {
+                "config_dir": constants.DEFAULT_CONFIG_DIR,
+                "languages_config_dir": os.path.join(constants.DEFAULT_CONFIG_DIR, constants.DEFAULT_LANGUAGES_CONFIG_DIR),
+                "generated_dir": constants.DEFAULT_GENERATED_CODE_DIR,
+                "spec_dir": constants.DEFAULT_SPEC_DIR,
+                "spec_v1_dir": os.path.join(constants.DEFAULT_SPEC_DIR, "v1"),
+                "template_patches_dir": constants.DEFAULT_TEMPLATE_PATCHES_DIR,
+                "templates_dir": constants.DEFAULT_TEMPLATES_DIR,
+            }
+            for _, v in dirs.items():
+                os.makedirs(v, exist_ok=True)
+            config_file = os.path.join(dirs["config_dir"], constants.DEFAULT_CONFIG_FILE)
+            if not os.path.exists(config_file):
+                with open(config_file, "w") as f:
+                    json.dump(
+                        {
+                            "codegen_exec": "openapi-generator",
+                            "languages": {},
+                            "server_base_urls": {
+                                "v1": "https://api.myserver.com/v1",
+                            },
+                            "spec_sections": {
+                                "v1": [],
+                            },
+                            "spec_versions": ["v1"],
+                        },
+                        f,
+                        indent=4,
+                    )
+            v1_header = os.path.join(dirs["spec_v1_dir"], constants.HEADER_FILE_NAME)
+            v1_shared = os.path.join(dirs["spec_v1_dir"], constants.SHARED_SECTION_NAME + ".yaml")
+            if not os.path.exists(v1_header):
+                with open(v1_header, "w") as f:
+                    yaml.dump(
+                        {
+                            "info": {
+                                "contact": {},
+                                "description": "Collection of all public API endpoints.",
+                                "title": "My API endpoints",
+                                "version": "1.0"
+                            },
+                            "openapi": "3.0.0",
+                        },
+                        f
+                    )
+            if not os.path.exists(v1_shared):
+                with open(v1_shared, "w") as f:
+                    yaml.dump(
+                        {
+                            "components": {
+                                "schemas": {},
+                                "security_schemes": {},
+                            },
+                            "security": {},
+                            "tags": [],
+                        },
+                        f
+                    )
+
+            if is_repo:
+                log.info("Creating a git repo in the new spec project directory")
+                run_command(["git", "init"], log_level=logging.DEBUG)
+                for d in [dirs["generated_dir"], dirs["templates_dir"]]:
+                    with open(os.path.join(d, ".gitkeep"), "w"):
+                        pass
+                if not os.path.exists(".gitignore"):
+                    with open(".gitignore", "w") as f:
+                        f.write(
+                            "generated/\n"
+                            "!generated/.gitkeep"
+                            "spec/*/full_spec.yaml\n"
+                            "templates/"
+                            "!templates/.gitkeep"
+                        )
+
+        return cmd_result
