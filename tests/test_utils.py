a423e9e5e65737694144e6b6dadc6d20c2405456
Laura <laura.hampton@datadoghq.com>
2019-11-12 14:26:51 -0800

    Add more unit tests to utils.py  (#71)
    * add test for change_cwd
    
    * add tests for get_current_commit
    
    * add test for validate_duplicates
    
    * add tests for fmt_cmd_out_for_log
    
    * add assertion to test_validate_duplicates
    
    * add tests for logging functions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,13 +2,13 @@ import copy
 import logging
 import os
 import subprocess
+import tempfile
 
 import flexmock
 import pytest
 
 from apigentools.constants import REDACTED_OUT_SECRET
-from apigentools.utils import env_or_val, log, run_command, set_log
-
+from apigentools.utils import change_cwd, env_or_val, fmt_cmd_out_for_log, get_current_commit, log, logging_enabled, run_command, set_log, set_log_level, validate_duplicates
 
 @pytest.mark.parametrize("env_var, default, args, typ, kwargs, set_env_to, expected", [
     ("APIGENTOOLS_TEST", "default", [], str, {}, None, "default"),
@@ -29,7 +29,6 @@ def test_env_or_val(env_var, default, args, typ, kwargs, set_env_to, expected):
         os.environ[env_var] = set_env_to
 
     assert env_or_val(env_var, default, *args, __type=typ, **kwargs) == expected
-
 
 def test_run_command(caplog):
     cmd = ["run", "this"]
@@ -65,3 +64,61 @@ def test_run_command(caplog):
     res = run_command(cmd, log_level=log_level, additional_env=additional_env, combine_out_err=combine_out_err)
     assert secret not in caplog.text
     assert REDACTED_OUT_SECRET in caplog.text
+
+def test_change_cwd():
+    present_dir = os.getcwd()
+    with tempfile.TemporaryDirectory() as target_dir:
+        with change_cwd(target_dir):
+            assert os.getcwd() == os.path.realpath(target_dir)
+        assert os.getcwd() == present_dir
+
+def test_get_current_commit(caplog):
+    with caplog.at_level(logging.WARNING):
+        with tempfile.TemporaryDirectory() as target_dir:
+            get_current_commit(target_dir)
+            for record in caplog.records:
+                assert "Failed getting current git commit" in record
+    flexmock(subprocess).should_receive("run").and_return(subprocess.CompletedProcess(1, 0, "some_hash"))
+    assert get_current_commit(".") == "some_hash"
+
+def test_validate_duplicates():
+    with pytest.raises(ValueError) as err:
+        validate_duplicates(["a", "b"], ["b", "c"])
+        assert "Duplicate field" in str(err.value)
+
+def test_fmt_cmd_out_for_log():
+    fake_CalledProcessError = flexmock.flexmock(returncode=1, stdout="stdout", stderr="stderr")
+    result = fmt_cmd_out_for_log(fake_CalledProcessError, combine_out_err=True)
+    assert result == 'RETCODE: 1\nOUTPUT:\nstdout'
+
+    result = fmt_cmd_out_for_log(fake_CalledProcessError, combine_out_err=False)
+    assert result == 'RETCODE: 1\nSTDOUT:\nstdoutSTDERR:\nstderr'
+
+    result = fmt_cmd_out_for_log(fake_CalledProcessError, combine_out_err=True)
+    assert result == 'RETCODE: 1\nOUTPUT:\nstdout'
+
+def test_logging_enabled(caplog):
+    with logging_enabled(enabled=False):
+        for record in caplog.records:
+            assert "NOTSET" in record
+
+    with logging_enabled(enabled=True):
+        for record in caplog.records:
+            assert "CRITICAL" in record
+
+def test_set_log_level(caplog):
+    set_log_level(log, "INFO")
+    for record in caplog.records:
+        assert "INFO" in record
+        assert "DEBUG" not in record
+
+def test_set_log_level_critical(caplog):
+    set_log_level(log, "CRITICAL")
+    for record in caplog.records:
+        assert "CRITICAL" in record
+        assert "INFO" not in record
+
+def test_set_log_default(caplog):
+    set_log(log)
+    for record in caplog.records:
+        assert "INFO" in record
