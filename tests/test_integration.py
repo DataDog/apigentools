29fa96d4de0766e468dd8645ac3183020ee72618
Laura Hampton <laura.hampton@datadoghq.com>
2019-11-13 11:46:40 -0500

    changed directory for pip install

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -11,7 +11,6 @@ FIXTURE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)),
 
 def test_setup():
     #pip install apigentools locally
-    # os.chdir("../apigentools") # dir is root of repo? - re
     result = subprocess.run(["pip", "install", "-e", os.curdir], check=True)
     assert result.returncode == 0
 
@@ -21,13 +20,13 @@ def test_init():
     with tempfile.TemporaryDirectory() as temp_dir:
         result = subprocess.run(["apigentools", "init", temp_dir], check=True)
         assert result.returncode == 0
-        #check that all the directories are present and correct
+        #check that all created directories are present and correct
         with os.scandir(temp_dir) as scan_results:
             dir_entry_list = [dir_entry.name for dir_entry in scan_results if dir_entry.is_dir() is True]
         assert sorted(dir_names) == sorted(dir_entry_list)
 
 
 def test_generate():
-    #testing that generate command throws an error if init is not run first
+    #ensure that generate command throws an error if init is not run first
     with pytest.raises(subprocess.CalledProcessError):
         result = subprocess.run(["apigentools", "generate"], check=True)
