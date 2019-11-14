23bca71289a7490fc07fbd3205e493917d5da48c
Laura Hampton <laura.hampton@datadoghq.com>
2019-11-14 12:04:39 -0500

    initial tests for init command

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -13,17 +13,21 @@ FIXTURE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)),
 def test_init():
     original_dir = os.getcwd()
     try:
-        dir_names = ["config", "generated", "template-patches", "downstream-templates", "spec", "templates", ".git"]
+        top_dir_names = ["config", "generated", "template-patches", "downstream-templates", "spec", "templates", ".git"]
         with tempfile.TemporaryDirectory() as temp_dir:
             os.chdir(temp_dir)
+            # TODO: check no_git_repo=True
             args = flexmock.flexmock(no_git_repo=False, projectdir=temp_dir)
             cmd_instance = InitCommand({}, args)
             cmd_instance.run()
             #check that all created directories are present and correct
             #TODO - check that subdirectories and yaml are also correct
-            with os.scandir(temp_dir) as scan_results:
-                dir_entry_list = [dir_entry.name for dir_entry in scan_results if dir_entry.is_dir() is True]
-            assert sorted(dir_names) == sorted(dir_entry_list)
+            dir_entries = [dir_entry[0] for dir_entry in os.walk(".")]
+            assert dir_entries == ['.', './generated', './template-patches', './config', './config/languages', './downstream-templates', './spec', './spec/v1', './templates', './.git', './.git/objects', './.git/objects/pack', './.git/objects/info', './.git/info', './.git/hooks', './.git/refs', './.git/refs/heads', './.git/refs/tags']
+
+            # with os.scandir(temp_dir) as scan_results:
+            #     dir_entry_list = [dir_entry.name for dir_entry in scan_results if dir_entry.is_dir() is True]
+            # assert sorted(top_dir_names) == sorted(dir_entry_list)
     # move back to original dir since tempdir is deleted on exiting with block
     except Exception as e:
         raise e
