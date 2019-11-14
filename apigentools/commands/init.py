6120b40f862a69baf53c53a74018d105eb041121
Laura Hampton <laura.hampton@datadoghq.com>
2019-11-14 15:57:29 -0500

    refactor InitCommand class so that file contents are importable for tests

--- apigentools/commands/init.py
+++ apigentools/commands/init.py
@@ -16,6 +16,51 @@ log = logging.getLogger(__name__)
 
 
 class InitCommand(Command):
+    def __init__(self, config, args):
+        self.config_file_json = {
+                                "codegen_exec": "openapi-generator",
+                                "languages": {},
+                                "server_base_urls": {
+                                    "v1": "https://api.myserver.com/v1",
+                                },
+                                "spec_sections": {
+                                    "v1": [],
+                                },
+                                "spec_versions": ["v1"],
+                            }
+        self.v1_header_json = {
+                                "info": {
+                                    "contact": {},
+                                    "description": "Collection of all public API endpoints.",
+                                    "title": "My API endpoints",
+                                    "version": "1.0"
+                                },
+                                "openapi": "3.0.0",
+                            }
+        self.v1_shared_json = {
+                                "components": {
+                                    "schemas": {},
+                                    "parameters": {},
+                                    "securitySchemes": {},
+                                    "requestBodies": {},
+                                    "responses": {},
+                                    "headers": {},
+                                    "examples": {},
+                                    "links": {},
+                                    "callbacks": {},
+                                },
+                                "security": [],
+                                "tags": [],
+                            }
+        self.gitignore = "!generated\n \
+                    generated/*\n \
+                    !generated/.gitkeep\n \
+                    spec/*/full_spec.yaml\n \
+                    !templates\n \
+                    templates/*\n  \
+                    !templates/.gitkeep"
+        super().__init__(config, args)
+
     def run(self):
         cmd_result = 0
         log.info("Initializing a new project directory")
@@ -38,58 +83,15 @@ class InitCommand(Command):
             config_file = os.path.join(dirs["config_dir"], constants.DEFAULT_CONFIG_FILE)
             if not os.path.exists(config_file):
                 with open(config_file, "w") as f:
-                    json.dump(
-                        {
-                            "codegen_exec": "openapi-generator",
-                            "languages": {},
-                            "server_base_urls": {
-                                "v1": "https://api.myserver.com/v1",
-                            },
-                            "spec_sections": {
-                                "v1": [],
-                            },
-                            "spec_versions": ["v1"],
-                        },
-                        f,
-                        indent=4,
-                    )
+                    json.dump(self.config_file_json,f,indent=4)
             v1_header = os.path.join(dirs["spec_v1_dir"], constants.HEADER_FILE_NAME)
             v1_shared = os.path.join(dirs["spec_v1_dir"], constants.SHARED_SECTION_NAME + ".yaml")
             if not os.path.exists(v1_header):
                 with open(v1_header, "w") as f:
-                    yaml.dump(
-                        {
-                            "info": {
-                                "contact": {},
-                                "description": "Collection of all public API endpoints.",
-                                "title": "My API endpoints",
-                                "version": "1.0"
-                            },
-                            "openapi": "3.0.0",
-                        },
-                        f
-                    )
+                    yaml.dump(self.v1_header_json, f)
             if not os.path.exists(v1_shared):
                 with open(v1_shared, "w") as f:
-                    yaml.dump(
-                        {
-                            "components": {
-                                "schemas": {},
-                                "parameters": {},
-                                "securitySchemes": {},
-                                "requestBodies": {},
-                                "responses": {},
-                                "headers": {},
-                                "examples": {},
-                                "links": {},
-                                "callbacks": {},
-                            },
-                            "security": [],
-                            "tags": [],
-                        },
-                        f
-                    )
-
+                    yaml.dump(self.v1_shared_json, f)
             if is_repo:
                 log.info("Creating a git repo in the new spec project directory")
                 run_command(["git", "init"], log_level=logging.DEBUG)
@@ -98,14 +100,5 @@ class InitCommand(Command):
                         pass
                 if not os.path.exists(".gitignore"):
                     with open(".gitignore", "w") as f:
-                        f.write(
-                            "!generated\n"
-                            "generated/*\n"
-                            "!generated/.gitkeep\n"
-                            "spec/*/full_spec.yaml\n"
-                            "!templates\n"
-                            "templates/*\n"
-                            "!templates/.gitkeep"
-                        )
-
+                        f.write(self.gitignore)
         return cmd_result
