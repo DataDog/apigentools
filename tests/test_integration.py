a168421ff3e8a9b1646997da59c878b394c98b1b
Laura Hampton <laura.hampton@datadoghq.com>
2019-11-14 11:07:26 -0500

    resolves file not found error

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -11,14 +11,21 @@ FIXTURE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)),
 
 
 def test_init():
-    dir_names = ["config", "generated", "template-patches", "downstream-templates", "spec", "templates", ".git"]
-    with tempfile.TemporaryDirectory() as temp_dir:
-        os.chdir(temp_dir)
-        args = flexmock.flexmock(no_git_repo=False, projectdir=temp_dir)
-        cmd_instance = InitCommand({}, args)
-        cmd_instance.run()
-        #check that all created directories are present and correct
-        #TODO - check that subdirectories and yaml are also correct
-        with os.scandir(temp_dir) as scan_results:
-            dir_entry_list = [dir_entry.name for dir_entry in scan_results if dir_entry.is_dir() is True]
-        assert sorted(dir_names) == sorted(dir_entry_list)
+    original_dir = os.getcwd()
+    try:
+        dir_names = ["config", "generated", "template-patches", "downstream-templates", "spec", "templates", ".git"]
+        with tempfile.TemporaryDirectory() as temp_dir:
+            os.chdir(temp_dir)
+            args = flexmock.flexmock(no_git_repo=False, projectdir=temp_dir)
+            cmd_instance = InitCommand({}, args)
+            cmd_instance.run()
+            #check that all created directories are present and correct
+            #TODO - check that subdirectories and yaml are also correct
+            with os.scandir(temp_dir) as scan_results:
+                dir_entry_list = [dir_entry.name for dir_entry in scan_results if dir_entry.is_dir() is True]
+            assert sorted(dir_names) == sorted(dir_entry_list)
+    # move back to original dir since tempdir is deleted on exiting with block
+    except Exception as e:
+        raise e
+    finally:
+        os.chdir(original_dir)
