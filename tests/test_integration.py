f518bb8cab08dbb3b1594feec8154034113a6718
Laura Hampton <laura.hampton@datadoghq.com>
2019-11-14 13:00:16 -0500

    some changes to init test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -8,34 +8,29 @@ from apigentools.commands.init import InitCommand
 # in case I need this later
 FIXTURE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)),
     'fixtures',)
+
 def test_init():
     original_dir = os.getcwd()
-    git_dirs = {'.', './generated', './template-patches', './config', './config/languages', './downstream-templates', './spec', './spec/v1', './templates', './.git', './.git/objects', './.git/objects/pack', './.git/objects/info', './.git/info', './.git/hooks', './.git/refs', './.git/refs/heads', './.git/refs/tags'}
-    no_git_dirs = {'.', './generated', './template-patches', './config', './config/languages', './downstream-templates', './spec', './spec/v1', './templates'}
-
-    try:
-        with tempfile.TemporaryDirectory() as temp_dir:
-            os.chdir(temp_dir)
-            args = flexmock.flexmock(no_git_repo=False, projectdir=temp_dir)
-            cmd_instance = InitCommand({}, args)
-            cmd_instance.run()
-            #TODO - check that subdirectories and yaml are also correct
-            dir_entries = set(dir_entry[0] for dir_entry in os.walk("."))
-            #sets do not presume the output of os.walk() will be ordered
-            assert dir_entries == git_dirs
-        #test --no-git-repo
-        with tempfile.TemporaryDirectory() as temp_dir:
-            os.chdir(temp_dir)
-            args = flexmock.flexmock(no_git_repo=True, projectdir=temp_dir)
-            cmd_instance = InitCommand({}, args)
-            cmd_instance.run()
-            #TODO - check that subdirectories and yaml are also correct
-            dir_entries = set(dir_entry[0] for dir_entry in os.walk("."))
-            #sets do not presume the output of os.walk() will be ordered
-            assert dir_entries == no_git_dirs
-
-    # move back to original dir since tempdir is deleted on exiting with block
-    except Exception as e:
-        raise e
-    finally:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        os.chdir(temp_dir)
+        args = flexmock.flexmock(no_git_repo=False, projectdir=temp_dir)
+        cmd_instance = InitCommand({}, args)
+        cmd_instance.run()
+        dir_entries = set(dir_entry[0] for dir_entry in os.walk("."))
+        #sets do not presume the output of os.walk() will be ordered
+        assert dir_entries == {'.', './generated', './template-patches', './config', './config/languages', './downstream-templates', './spec', './spec/v1', './templates', './.git', './.git/objects', './.git/objects/pack', './.git/objects/info', './.git/info', './.git/hooks', './.git/refs', './.git/refs/heads', './.git/refs/tags'}
+        # move back to original dir since tempdir is deleted on exiting with block
         os.chdir(original_dir)
+
+    #test --no-git-repo
+    with tempfile.TemporaryDirectory() as temp_dir:
+        os.chdir(temp_dir)
+        args = flexmock.flexmock(no_git_repo=True, projectdir=temp_dir)
+        cmd_instance = InitCommand({}, args)
+        cmd_instance.run()
+        dir_entries = set(dir_entry[0] for dir_entry in os.walk("."))
+        assert dir_entries == {'.', './generated', './template-patches', './config', './config/languages', './downstream-templates', './spec', './spec/v1', './templates'}
+        os.chdir(original_dir)
+
+        #TODO - check that subdirectories and yaml are also correct
+
