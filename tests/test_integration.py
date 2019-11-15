6aab127adff030bfe6150a0524cd428650efecbb
Laura Hampton <laura.hampton@datadoghq.com>
2019-11-15 12:54:53 -0500

    add tmpdir fixture in first test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -10,26 +10,26 @@ from apigentools.commands.init import InitCommand
 FIXTURE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)),
     'fixtures',)
 
-def test_init():
+def test_init(tmpdir):
     original_dir = os.getcwd()
     try:
-        with tempfile.TemporaryDirectory() as temp_dir:
-            os.chdir(temp_dir)
-            args = flexmock.flexmock(no_git_repo=False, projectdir=temp_dir)
-            cmd_instance = InitCommand({}, args)
-            cmd_instance.run()
-            #sets do not presume the output of os.walk() will be ordered
-            dir_entries = set(dir_entry[0] for dir_entry in os.walk("."))
-            # the layout of git repos has changed over time, this dir may or may not be present
-            if "./.git/branches" in dir_entries:
-                dir_entries.remove("./.git/branches")
-            assert dir_entries == {
-                '.', './generated', './template-patches',
-                './config', './config/languages', './downstream-templates',
-                './spec', './spec/v1', './templates', './.git',
-                './.git/objects', './.git/objects/pack',
-                './.git/objects/info', './.git/info', './.git/hooks',
-                './.git/refs', './.git/refs/heads', './.git/refs/tags'
+        temp_dir = tmpdir.mkdir("test_init_git_dir")
+        os.chdir(temp_dir)
+        args = flexmock.flexmock(no_git_repo=False, projectdir=temp_dir)
+        cmd_instance = InitCommand({}, args)
+        cmd_instance.run()
+        #sets do not presume the output of os.walk() will be ordered
+        dir_entries = set(dir_entry[0] for dir_entry in os.walk("."))
+        # the layout of git repos has changed over time, this dir may or may not be present
+        if "./.git/branches" in dir_entries:
+            dir_entries.remove("./.git/branches")
+        assert dir_entries == {
+            '.', './generated', './template-patches',
+            './config', './config/languages', './downstream-templates',
+            './spec', './spec/v1', './templates', './.git',
+            './.git/objects', './.git/objects/pack',
+            './.git/objects/info', './.git/info', './.git/hooks',
+            './.git/refs', './.git/refs/heads', './.git/refs/tags'
                 }
 
     # move back to original dir since temp_dir is deleted on exiting with block
