d36c68d6c718d9fc57f379e685220140173f735c
Laura Hampton <laura.hampton@datadoghq.com>
2019-11-18 16:19:24 -0500

    linter

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -6,19 +6,17 @@ import pytest
 from apigentools.commands.init import InitCommand
 
 
-
-
 def test_init(tmpdir):
     # directories created by init command
     directory_names = {
-    'generated',
-    'template-patches',
-    'config',
-    os.path.join('config', 'languages'),
-    'downstream-templates',
-    'spec',
-    os.path.join('spec', 'v1'),
-    'templates',
+        'generated',
+        'template-patches',
+        'config',
+        os.path.join('config', 'languages'),
+        'downstream-templates',
+        'spec',
+        os.path.join('spec', 'v1'),
+        'templates',
     }
     temp_dir = tmpdir.mkdir("test_init_git_dir")
     args = flexmock.flexmock(no_git_repo=False, projectdir=temp_dir)
@@ -39,7 +37,8 @@ def test_init(tmpdir):
     cmd_instance = InitCommand({}, args)
     cmd_instance.run()
     dir_entries = set(dir_entry[0] for dir_entry in os.walk(temp_dir))
-    # create a set of expected directory names to test against, and add the root temp_dir since it's in the output of `os.walk()`
+    # create a set of expected directory names to test against, and add the
+    # root temp_dir since it's in the output of `os.walk()`
     test_dirs = set(os.path.join(temp_dir, name) for name in directory_names)
     test_dirs.add(temp_dir)
     assert dir_entries == test_dirs
