f8693bdb1399a1b314219f673f9de5235d60d030
Laura Hampton <laura.hampton@datadoghq.com>
2019-11-18 16:17:03 -0500

    refactors out list of directory names created by init command in test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -5,44 +5,33 @@ import pytest
 
 from apigentools.commands.init import InitCommand
 
-temp_dir = "/temp_dir"
-TEST_DIRS = {
-    temp_dir,
-    os.path.join(temp_dir, 'generated'),
-    os.path.join(temp_dir, 'template-patches'),
-    os.path.join(temp_dir, 'config'),
-    os.path.join(temp_dir, 'config/languages'),
-    os.path.join(temp_dir, 'downstream-templates'),
-    os.path.join(temp_dir, 'spec'),
-    os.path.join(temp_dir, 'spec/v1'),
-    os.path.join(temp_dir, 'templates'),
-}
+
+
 
 def test_init(tmpdir):
+    # directories created by init command
+    directory_names = {
+    'generated',
+    'template-patches',
+    'config',
+    os.path.join('config', 'languages'),
+    'downstream-templates',
+    'spec',
+    os.path.join('spec', 'v1'),
+    'templates',
+    }
     temp_dir = tmpdir.mkdir("test_init_git_dir")
     args = flexmock.flexmock(no_git_repo=False, projectdir=temp_dir)
     cmd_instance = InitCommand({}, args)
     cmd_instance.run()
     # sets do not presume the output of os.walk() will be ordered
     dir_entries = set(dir_entry[0] for dir_entry in os.walk(temp_dir))
-    # the layout of git repos has changed over time, so only look for the top
-    # .git directory
-    # test_dirs = {
-    # temp_dir,
-    # os.path.join(temp_dir, 'generated'),
-    # os.path.join(temp_dir, 'template-patches'),
-    # os.path.join(temp_dir, 'config'),
-    # os.path.join(temp_dir, 'config/languages'),
-    # os.path.join(temp_dir, 'downstream-templates'),
-    # os.path.join(temp_dir, 'spec'),
-    # os.path.join(temp_dir, 'spec/v1'),
-    # os.path.join(temp_dir, 'templates'),
-    # os.path.join(temp_dir, '.git'),
-    # }
-    TEST_DIRS = TEST_DIRS.add(os.path.join(temp_dir, '.git'))
-    assert TEST_DIRS.issubset(dir_entries)
-
-    # assert test_dirs.issubset(dir_entries)
+    # create a set of expected directory names to test against
+    test_dirs = set(os.path.join(temp_dir, name) for name in directory_names)
+    # the contents of git repos has changed over time, so only look for the top
+    test_dirs.add(os.path.join(temp_dir, '.git'))
+    # import pdb; pdb.set_trace()
+    assert test_dirs.issubset(dir_entries)
 
     # test --no-git-repo
     temp_dir = tmpdir.mkdir("test_init_no_git_dir")
@@ -50,15 +39,7 @@ def test_init(tmpdir):
     cmd_instance = InitCommand({}, args)
     cmd_instance.run()
     dir_entries = set(dir_entry[0] for dir_entry in os.walk(temp_dir))
-    test_dirs = {
-    temp_dir,
-    os.path.join(temp_dir, 'generated'),
-    os.path.join(temp_dir, 'template-patches'),
-    os.path.join(temp_dir, 'config'),
-    os.path.join(temp_dir, 'config/languages'),
-    os.path.join(temp_dir, 'downstream-templates'),
-    os.path.join(temp_dir, 'spec'),
-    os.path.join(temp_dir, 'spec/v1'),
-    os.path.join(temp_dir, 'templates')
-        }
+    # create a set of expected directory names to test against, and add the root temp_dir since it's in the output of `os.walk()`
+    test_dirs = set(os.path.join(temp_dir, name) for name in directory_names)
+    test_dirs.add(temp_dir)
     assert dir_entries == test_dirs
