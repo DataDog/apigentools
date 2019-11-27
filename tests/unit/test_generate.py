4c7c7eaeb803ba98880c4de40eae9a6de66df9cb
Laura Hampton <laura.hampton@datadoghq.com>
2019-11-27 15:26:15 -0500

    add raw_dict to test fixtures

--- tests/unit/test_generate.py
+++ tests/unit/test_generate.py
@@ -13,7 +13,6 @@ from apigentools.utils import run_command
 FIXTURE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "fixtures",)
 
 
-# pull_repository
 # render_downstream_templates
 # run_language_commands
 
@@ -45,17 +44,9 @@ def test_get_image_name():
 
 def test_get_codegen_version():
     args = flexmock()
-    cfg = Config(
-        {
-            "codegen_exec": "openapi-generator",
-            "languages": {},
-            "server_base_urls": {},
-            "spec_sections": {},
-            "spec_versions": [],
-            "generate_extra_args": [],
-            "user_agent_client_name": "OpenAPI",
-        }
-    )
+    with open(os.path.join(FIXTURE_DIR, "raw_dict.json"), "r") as f:
+        raw_dict = json.loads(f.read())
+    cfg = Config(raw_dict)
     cmd = GenerateCommand(cfg, args)
     expected_result = run_command([cmd.config.codegen_exec, "version"])
     expected_result = expected_result.stdout.strip()
@@ -65,47 +56,26 @@ def test_get_codegen_version():
 
 def test_get_stamp():
     args = flexmock(generated_with_image=None, spec_repo_dir=".", additional_stamp=[])
-    cfg = Config(
-        {
-            "codegen_exec": "openapi-generator",
-            "languages": {},
-            "server_base_urls": {},
-            "spec_sections": {},
-            "spec_versions": [],
-            "generate_extra_args": [],
-            "user_agent_client_name": "OpenAPI",
-        }
-    )
+    with open(os.path.join(FIXTURE_DIR, "raw_dict.json"), "r") as f:
+        raw_dict = json.loads(f.read())
+    cfg = Config(raw_dict)
     cmd = GenerateCommand(cfg, args)
     stamp = cmd.get_stamp()
-    # import pdb; pdb.set_trace()
     assert "Generated with" in stamp
     assert "codegen version" in stamp
 
-# @pytest.mark.skip
-# mock git pull command
+
 def test_pull_repository(tmpdir):
     temp_dir = tmpdir.mkdir("generated_lang_dir")
     args = flexmock(action='generate', additional_stamp=[], api_versions=None, builtin_templates=False, clone_repo=False, config_dir='config', downstream_templates_dir=temp_dir, full_spec_file='full_spec.yaml', generated_code_dir=temp_dir, generated_with_image=None, git_via_https=True, git_via_https_installation_access_token='', git_via_https_oauth_token='', languages=None, spec_dir='spec', spec_repo_dir='.', template_dir='templates', verbose=False, github_repo_name="apigentools")
-    raw_dict = {
-        "codegen_exec": "openapi-generator",
-        "languages": {"java":{"github_repo_name": "repo_name"}},
-        "server_base_urls": {},
-        "spec_sections": {},
-        "spec_versions": [],
-        "generate_extra_args": [],
-        "user_agent_client_name": "OpenAPI",
-        "github_repo_name" : "apigentools",
-        "github_org_name" : "DataDog"
-    }
+    with open(os.path.join(FIXTURE_DIR, "raw_dict.json"), "r") as f:
+        raw_dict = json.loads(f.read())
     cfg = Config(raw_dict)
     language = LanguageConfig(language="java", raw_dict=raw_dict, top_level_config=cfg)
-    # flexmock(subprocess).should_receive("run").\
-    # and_return(subprocess.CompletedProcess(1, 0))
     cmd = GenerateCommand(cfg, args)
     # git pull of https://github.com/DataDog/apigentools.git
     cmd.pull_repository(language)
-    # make a list of the names of the directories in the repo
+    # make a list of the names of the directories from the repo
     dir_contents = [el[1] for el in os.walk(temp_dir)]
     assert ['.azure-pipelines', 'apigentools', 'tests', 'docs', 'hooks', '.github', '.git'] in dir_contents
 
@@ -115,18 +85,11 @@ def test_pull_repository(tmpdir):
 
 def test_write_dot_apigentools_info(tmpdir):
     temp_dir = tmpdir.mkdir("generated")
-    repo_dir = tmpdir.mkdir("generated/repo_name")
+    repo_dir = tmpdir.mkdir("generated/DataDog")
 
     args = flexmock(action='generate', additional_stamp=[], api_versions=None, builtin_templates=False, clone_repo=False, config_dir='config', downstream_templates_dir='downstream-templates', full_spec_file='full_spec.yaml', generated_code_dir=temp_dir, generated_with_image=None, git_via_https=False, git_via_https_installation_access_token='', git_via_https_oauth_token='', languages=None, spec_dir='spec', spec_repo_dir='.', template_dir='templates', verbose=False, github_repo_name="repo_name")
-    raw_dict = {
-        "codegen_exec": "openapi-generator",
-        "languages": {"java":{"github_repo_name": "repo_name"}},
-        "server_base_urls": {},
-        "spec_sections": {},
-        "spec_versions": [],
-        "generate_extra_args": [],
-        "user_agent_client_name": "OpenAPI",
-    }
+    with open(os.path.join(FIXTURE_DIR, "raw_dict.json"), "r") as f:
+        raw_dict = json.loads(f.read())
     cfg = Config(raw_dict)
     cmd = GenerateCommand(cfg, args)
     language_cfg = LanguageConfig(language="java", raw_dict=raw_dict, top_level_config=cfg)
