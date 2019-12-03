68bedf9ff580931e7a2b0bef0dff5143aa072b78
Jiri Kuncar <jiri.kuncar@gmail.com>
2019-12-03 19:47:55 +0100

    Fetch branch from origin

--- apigentools/commands/generate.py
+++ apigentools/commands/generate.py
@@ -354,6 +354,7 @@ class GenerateCommand(Command):
             )
             if branch is not None:
                 try:
+                    run_command(["git", "fetch", "origin", branch], cwd=output_dir)
                     run_command(["git", "checkout", branch], cwd=output_dir)
                 except subprocess.CalledProcessError:
                     # if the branch doesn't exist, we stay in the default one
