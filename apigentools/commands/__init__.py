46434612e9131cdc6e86dcf0bd5593dfd9e94640
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2020-01-02 11:03:30 -0500

    Address review

--- apigentools/commands/__init__.py
+++ apigentools/commands/__init__.py
@@ -1,7 +1,7 @@
 # Unless explicitly stated otherwise all files in this repository are licensed
 # under the 3-clause BSD style license (see LICENSE).
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
-# Copyright 2019-Present, Inc.
+# Copyright 2019-Present Datadog, Inc.
 from apigentools.commands.generate import GenerateCommand
 from apigentools.commands.init import InitCommand
 from apigentools.commands.push import PushCommand
